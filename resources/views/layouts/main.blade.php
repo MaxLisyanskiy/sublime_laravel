e6ab265c44dca03ade99c1fb362afe170d0baf0b
Максим Лисянский <max_lisyanskiy@mail.ru>
2021-07-07 23:59:47 +0300

    add categories

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -43,11 +43,9 @@
                                     <li class="hassubs">
                                         <a href="categories.html">Categories</a>
                                         <ul>
-                                            <li><a href="categories.html">Category</a></li>
-                                            <li><a href="categories.html">Category</a></li>
-                                            <li><a href="categories.html">Category</a></li>
-                                            <li><a href="categories.html">Category</a></li>
-                                            <li><a href="categories.html">Category</a></li>
+                                            @foreach($categories)
+                                            <li><a href="categories.html">{{$categories->title}}</a></li>
+                                            @endforeach
                                         </ul>
                                     </li>
                                     <li><a href="#">Accessories</a></li>
